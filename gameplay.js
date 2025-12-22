e1bd447fedcedc9283c9481710c1dda23789810c
Frederick <legendsof2@gmail.com>
2025-12-23 00:14:42 +0100

    Implemented tile replacement turn

--- gameplay.js
+++ gameplay.js
@@ -1,42 +1,76 @@
 let letters = [
-    "A", "A", "A", "A", "A", "A", "A", "A", "A",
-    "B", "B",
-    "C", "C",
-    "D", "D", "D", "D",
-    "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
-    "F", "F",
-    "G", "G", "G",
-    "H", "H",
-    "I", "I", "I", "I", "I", "I", "I", "I", "I",
+    "A", "A", "A", "A",
+    "B",
+    "C",
+    "D", "D",
+    "E", "E", "E", "E", "E", "E",
+    "F",
+    "G",
+    "H",
+    "I", "I", "I", "I",
     "J",
     "K",
-    "L", "L", "L", "L",
-    "M", "M",
-    "N", "N", "N", "N", "N", "N",
-    "O", "O", "O", "O", "O", "O", "O", "O",
-    "P", "P",
+    "L", "L",
+    "M",
+    "N", "N", "N",
+    "O", "O", "O", "O",
+    "P",
     "Q",
-    "R", "R", "R", "R", "R", "R",
-    "S", "S", "S", "S",
-    "T", "T", "T", "T", "T", "T",
-    "U", "U", "U", "U",
-    "V", "V",
-    "W", "W",
+    "R", "R",
+    "S", "S",
+    "T", "T",
+    "U", "U",
+    "V",
+    "W",
     "X",
-    "Y", "Y",
+    "Y",
     "Z",
-    " ", " "
+    " "
 ];
+
 shuffle(letters);
+
+const lookupTable = {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D",
+    "E": "E",
+    "F": "F",
+    "G": "G",
+    "H": "H",
+    "I": "I",
+    "J": "J",
+    "K": "K",
+    "L": "L",
+    "M": "M",
+    "N": "N",
+    "O": "O",
+    "P": "P",
+    "Q": "Q",
+    "R": "R",
+    "S": "S",
+    "T": "T",
+    "U": "U",
+    "V": "V",
+    "W": "W",
+    "X": "X",
+    "Y": "Y",
+    "Z": "Z",
+    " ": " "
+};
 
 let fields = [];
 for(let i = 0; i < 15; i++) { fields[i] = []; }
 
 let first_move_flag = false;
+let tiles_placed_flag = false;
 
 class GameState {
     static SELECT_HAND_TILE = 0;
     static SELECT_BOARD_TILE = 1;
+    static SELECT_REPLACE_TILES = 2;
+    static GAME_OVER = 3;
 }
 
 let CurrentGameState = GameState.SELECT_HAND_TILE;
@@ -44,13 +78,21 @@ let CurrentGameState = GameState.SELECT_HAND_TILE;
 function draw() {
     $(".handtile").each(function() {
         if($(this).text() === "--") {
-            $(this).text(letters.pop())
+            let tile = letters.pop();
+            if(tile === undefined) {
+                CurrentGameState = GameState.GAME_OVER;
+                alert("Game Over!");
+                return;
+            }
+            $(this).text(tile)
                 .css("background-color","rgb(238, 220, 170)")
                 .addClass("active-handtile").on("click", activeHandTileOnClick)
                 .on("mouseenter", activeHandTileOnMouseEnter)
                 .on("mouseleave", activeHandTileOnMouseLeave);
         }
     });
+    tiles_placed_flag = false;
+    $("#redraw-button").removeAttr("disabled");
 }
 
 draw();
@@ -58,6 +100,8 @@ draw();
 let selectedHandTile = "";
 
 function activeHandTileOnClick() {
+
+    if(CurrentGameState === GameState.SELECT_REPLACE_TILES) selectReplacementTiles(this);
 
     if(CurrentGameState !== GameState.SELECT_HAND_TILE
         && CurrentGameState !== GameState.SELECT_BOARD_TILE) return;
@@ -73,7 +117,10 @@ function activeHandTileOnClick() {
 function activeHandTileOnMouseEnter() {
 
     if(CurrentGameState !== GameState.SELECT_HAND_TILE
-        && CurrentGameState !== GameState.SELECT_BOARD_TILE) return;
+        && CurrentGameState !== GameState.SELECT_BOARD_TILE
+        && CurrentGameState !== GameState.SELECT_REPLACE_TILES) return;
+
+    if($(this).css("background-color") === "rgb(255, 0, 0)") return;
 
     $(this).css("background-color", "rgb(250,208,103)")
         .css("cursor", "pointer");
@@ -81,6 +128,10 @@ function activeHandTileOnMouseEnter() {
 }
 
 function activeHandTileOnMouseLeave() {
+
+    if(CurrentGameState !== GameState.SELECT_HAND_TILE
+        && CurrentGameState !== GameState.SELECT_BOARD_TILE
+        && CurrentGameState !== GameState.SELECT_REPLACE_TILES) return;
 
     if($(this).css("background-color") === "rgb(255, 0, 0)") return;
 
@@ -127,11 +178,8 @@ function validTileOnClick() {
         .addClass("placed-tile")
         .text(selectedHandTileObj.text());
 
-    selectedHandTileObj.text("--")
-        .css("background-color", "rgb(200,200,200)")
-        .css("cursor", "")
-        .removeClass("active-handtile")
-        .off("click mouseenter mouseleave");
+    unsetHandtile("#" + selectedHandTile);
+
     $(".active-handtile").css("background-color", "rgb(238, 220, 170)");
 
     for(let i = 0; i < neighbourTiles.length; i++) {
@@ -142,9 +190,11 @@ function validTileOnClick() {
             .on("mouseleave", validTileOnMouseLeave);
     }
 
-    CurrentGameState = GameState.SELECT_HAND_TILE;
+    $("#redraw-button").attr("disabled", "disabled");
 
+    CurrentGameState = GameState.SELECT_HAND_TILE;
     first_move_flag = true;
+    tiles_placed_flag = true;
 
 }
 
@@ -152,13 +202,61 @@ $(".valid-tile").on("mouseenter", validTileOnMouseEnter)
     .on("mouseleave", validTileOnMouseLeave)
     .on("click", validTileOnClick);
 
-
 function endTurn() {
+
+    if(CurrentGameState === GameState.SELECT_HAND_TILE
+        || CurrentGameState === GameState.SELECT_BOARD_TILE) {
+        draw();
+        return;
+    }
+
+    if(CurrentGameState === GameState.SELECT_REPLACE_TILES) {
+        $(".selected-handtile").each(function() {
+            letters.push($(this).text());
+            unsetHandtile(this);
+        });
+        shuffle(letters);
+        draw();
+        $("#redraw-button").css("background-color", "");
+
+        CurrentGameState = GameState.SELECT_HAND_TILE;
+        return;
+    }
+}
+
+function redraw() {
 
     if(CurrentGameState !== GameState.SELECT_HAND_TILE
         && CurrentGameState !== GameState.SELECT_BOARD_TILE) return;
+    if(tiles_placed_flag) return;
 
-    draw();
+    $(".active-handtile").css("background-color", "rgb(238, 220, 170)")
+        .removeClass("selected-handtile");
+
+    $("#redraw-button").css("background-color", "red");
+
+    CurrentGameState = GameState.SELECT_REPLACE_TILES;
+
+}
+
+function selectReplacementTiles(obj) {
+
+    if($(obj).css("background-color") === "rgb(255, 0, 0)") {
+        $(obj).css("background-color", "")
+            .removeClass("selected-handtile");
+    } else {
+        $(obj).css("background-color", "rgb(255, 0, 0)")
+            .addClass("selected-handtile");
+    }
+
+}
+
+function unsetHandtile(tile) {
+    $(tile).text("--")
+        .css("background-color", "rgb(200,200,200)")
+        .css("cursor", "")
+        .removeClass("active-handtile")
+        .off("click mouseenter mouseleave");
 }
 
 /********************
